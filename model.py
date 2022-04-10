05da5dfc0b56741db47823de80b1189f97f00080
Saeed Abbasi <saeedabc@tiger.eecs.yorku.ca>
2022-04-10 16:15:43 -0400

    merging batches bug fix

--- model.py
+++ model.py
@@ -8,22 +8,22 @@ from transformers import AutoModel, BertModel
 
 
 class QAGNN(nn.Module):
-    def __init__(self, lm_name, hid_dim, n_ntype, n_etype, dropout):
+    def __init__(self, lm_name, cp_dim, hid_dim, n_ntype, n_etype, dropout):
         super(QAGNN, self).__init__()
 
         self.text_enc = AutoModel.from_pretrained(lm_name, output_hidden_states=True)
         for param in self.text_enc.base_model.parameters():
             param.requires_grad = False
 
-        self.qa_dim = self.text_enc.config.hidden_size
-        self.gnn = GNN(self.qa_dim, hid_dim, n_ntype, n_etype, dropout)
+        qa_dim = self.text_enc.config.hidden_size
+        self.gnn = GNN(qa_dim, cp_dim, hid_dim, n_ntype, n_etype, dropout)
 
         self.mlp = nn.Sequential(
-            nn.Linear(self.qa_dim + 2 * hid_dim, 2 * hid_dim),
+            nn.Linear(qa_dim + 2 * hid_dim, 2 * hid_dim),
             nn.ReLU(),
             nn.Linear(2 * hid_dim, hid_dim // 2),
             nn.ReLU(),
-            nn.Linear(hid_dim//2, 1)
+            nn.Linear(hid_dim // 2, 1)
             # nn.Sigmoid()
         )
 
@@ -31,12 +31,14 @@ class QAGNN(nn.Module):
         input_ids, input_masks, segment_ids, output_masks = tbatch
         text_hid_states = self.text_enc(input_ids=input_ids, token_type_ids=segment_ids, attention_mask=input_masks)
         last_hid_states = text_hid_states[-1][-1]
-        qa_emb = last_hid_states.mean(dim=0)  # (tb, qa_dim,)
-        qa_emb = Batch.from_data_list(data_list=qa_emb, follow_batch=gbatch.batch)  # ([gb,] qa_dim,)
-
+        qa_emb = last_hid_states.mean(dim=1)  # (tb, qa_dim,)
+        
+        # qa_emb = Batch.from_data_list(data_list=list(qa_emb), follow_batch=gbatch.batch)  # ([gb,] qa_dim,)
+        # qa_emb = Batch(qa_emb, follow_batch=gbatch.batch)
+        # print(qa_emb.size(), gbatch.x.size())
         qa_node_emb, pooled_graph_emb = self.gnn(qa_emb=qa_emb, x=gbatch.x, node_ids=gbatch.node_ids, node_types=gbatch.node_types, node_scores=gbatch.node_scores,
                         edge_index=gbatch.edge_index, edge_type=gbatch.edge_type, edge_attr=gbatch.edge_attr, node2graph=gbatch.batch)  # (hid_dim,), (hid_dim,)
-
+        
         emb = torch.concat([qa_emb, qa_node_emb, pooled_graph_emb], dim=-1)
         emb = self.mlp(emb)
 
@@ -44,17 +46,18 @@ class QAGNN(nn.Module):
 
 
 class GNN(nn.Module):
-    def __init__(self, qa_dim, hid_dim, n_ntype, n_etype, dropout):
+    def __init__(self, qa_dim, cp_dim, hid_dim, n_ntype, n_etype, dropout):
         super(GNN, self).__init__()
         self.hid_dim = hid_dim
         self.dropout = dropout
         self.act = nn.ReLU()
         # self.gelu
 
-        self.qa2node = nn.Linear(qa_dim, hid_dim)
-        # self.x2h = nn.Linear(x_init_dim, hid_dim)
+        self.qa2node = nn.Linear(qa_dim, cp_dim)
+        
         self.ntype_nscore_enc = nn.Linear(n_ntype + 1, hid_dim // 2)
 
+        self.x2h = nn.Linear(cp_dim + hid_dim // 2, hid_dim)
         # self.h2h = nn.Linear(2 * hid_dim, hid_dim)
 
         self.etype_enc = nn.Sequential(
@@ -65,24 +68,41 @@ class GNN(nn.Module):
             self.act
         )
 
-        gc_hid_dim = hid_dim + hid_dim // 2
-        self.gat_conv = GATConv(gc_hid_dim, hid_dim, add_self_loops=False, edge_dim=hid_dim)
+        # gc_hid_dim = cp_dim + hid_dim // 2
+        self.gat_conv = GATConv(hid_dim, hid_dim, add_self_loops=False, edge_dim=hid_dim)
 
     def forward(self, qa_emb, x, node_ids, node_types, node_scores, edge_index, edge_type, edge_attr, node2graph):
-        x[0, :] = self.qa2node(qa_emb)
-        node_extras = self.ntype_nscore_enc(torch.cat([node_types, node_scores], dim=-1))  # n_ntype + 1 --> D/2
-        x = torch.cat([x, node_extras], dim=-1)  # 2D --> D
+        def _working_graph(qa_emb, x):
+            # print(qa_emb.size(), x.size())
+            bs = qa_emb.size(0)
+            n_nodes_pb = x.size(0) // bs
+            for b_idx in range(bs):
+                qa_node_idx = b_idx * n_nodes_pb
+                x[qa_node_idx, :] = qa_emb[b_idx, :]
+            return x
+
+        def _extract_h0(h, bs):
+            h0_selector = range(0, h.size(0), h.size(0) // bs)
+            return h[h0_selector, :]
+        
+        x = _working_graph(qa_emb=self.qa2node(qa_emb), x=x)
+        x_extras = self.ntype_nscore_enc(torch.cat([node_types, node_scores], dim=-1))  # n_ntype + 1 --> D/2
+        x = torch.cat([x, x_extras], dim=-1)  # 2D --> D
         x = self.act(x)
+        
+        h = self.act(self.x2h(x))
 
         edge_attr = self.etype_enc(edge_attr)
 
-        x = self.gat_conv(x=x, edge_index=edge_index, edge_attr=edge_attr)
+        h = self.gat_conv(x=h, edge_index=edge_index, edge_attr=edge_attr)
         
-        p = global_mean_pool(x, batch=node2graph).squeeze()
+        p = global_mean_pool(h, batch=node2graph)
         # p = self.act(p)
-
         # x = F.dropout(x, p=self.dropout, training=self.training)
-        return x[0], p
+        
+        h0 = _extract_h0(h, bs=qa_emb.size(0))
+        
+        return h0, p
 
 
 # class CustomMessagePassing(MessagePassing):
