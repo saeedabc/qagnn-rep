9628326864a212f36e9cb3211b8c3e8dcbbf5189
Saeed Abbasi <saeedabc@tiger.eecs.yorku.ca>
2022-04-11 03:35:17 -0400

    sync

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from data_loader import QAGNN_RawDataLoader
 from model import QAGNN
 
 
-def main(mode, seed, lr, batch_size, n_epochs, n_ntype, n_etype, max_n_nodes, max_seq_len, hid_dim, dropout, weight_decay, lm_name,
+def main(mode, seed, lr, batch_size, n_epochs, eval_every_n_steps, n_ntype, n_etype, max_n_nodes, max_seq_len, hid_dim, dropout, weight_decay, lm_name,
          cp_emb_path, train_adj_path, train_stmt_path, dev_adj_path, dev_stmt_path, test_adj_path, test_stmt_path, **cfg):
 
     random.seed(seed)
@@ -42,14 +42,14 @@ def main(mode, seed, lr, batch_size, n_epochs, n_ntype, n_etype, max_n_nodes, ma
 
     if 'train' in mode:
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay)
-        scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=lr, max_lr=0.1, step_size_up=200, mode="triangular2")
+        scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, base_lr=lr, max_lr=3e-2, step_size_up=250, mode="triangular2")
 
         train_text_ds, train_graph_ds = db.train_dataset()
         train_dls = DataLoader(train_text_ds, batch_size=batch_size, shuffle=True), GraphDataLoader(dataset=train_graph_ds, batch_size=batch_size, shuffle=True)
         dev_text_ds, dev_graph_ds = db.dev_dataset()
         dev_dls = DataLoader(dev_text_ds, batch_size=batch_size, shuffle=True), GraphDataLoader(dataset=dev_graph_ds, batch_size=batch_size, shuffle=True)
 
-        train(device, model, criterion, optimizer, scheduler, batch_size, train_loaders=train_dls, dev_loaders=dev_dls, n_epochs=n_epochs)
+        train(device, model, criterion, optimizer, scheduler, batch_size, train_loaders=train_dls, dev_loaders=dev_dls, n_epochs=n_epochs, eval_every_n_steps=eval_every_n_steps)
 
         pathlib.Path('/'.join(save_path.split('/')[:-1])).mkdir(parents=True, exist_ok=True)
         torch.save(model.state_dict(), save_path)
@@ -62,7 +62,7 @@ def main(mode, seed, lr, batch_size, n_epochs, n_ntype, n_etype, max_n_nodes, ma
         evaluate(device, model, criterion, loaders=test_dls)
 
 
-def train(device, model, criterion, optimizer, scheduler, batch_size, train_loaders, dev_loaders, n_epochs, print_every_n_steps=25):
+def train(device, model, criterion, optimizer, scheduler, batch_size, train_loaders, dev_loaders, n_epochs, eval_every_n_steps):
     model.train()
 
     n_batches = len(train_loaders[1])
@@ -95,19 +95,19 @@ def train(device, model, criterion, optimizer, scheduler, batch_size, train_load
 
             with torch.no_grad():
                 pred = torch.round(torch.sigmoid(out))
-                n_correct += torch.sum(pred == target)
+                n_correct += torch.sum(pred == target).item()
                 n_total += gbatch.num_graphs
-                running_loss += loss
+                running_loss += loss.item()
 
                 step = epoch * n_batches + i
-                if (step + 1) % print_every_n_steps == 0:
-                    train_avg_acc = n_correct / n_total
+                if (step + 1) % eval_every_n_steps == 0:
+                    train_avg_acc = 100 * (n_correct / n_total)
                     acc_list.append(train_avg_acc)
 
-                    train_avg_loss = running_loss / print_every_n_steps
+                    train_avg_loss = running_loss / eval_every_n_steps
                     loss_list.append(train_avg_loss)
 
-                    print(f'Epoch[{epoch}], Step[{step + 1}], Avg Train Acc: {train_avg_acc}, Avg Train Loss: {train_avg_loss}')
+                    print(f'Epoch[{epoch}], Step[{step + 1}], Avg Train Acc: {train_avg_acc:.4f}, Avg Train Loss: {train_avg_loss:.4f}')
 
                     dev_avg_acc, dev_avg_loss = evaluate(device, model, criterion, dev_loaders)
                     dev_acc_list.append(dev_avg_acc)
@@ -137,18 +137,19 @@ def evaluate(device, model, criterion, loaders):
             loss = criterion(out, target)
 
             pred = torch.round(torch.sigmoid(out))
-            n_correct += torch.sum(pred == target)
+            n_correct += torch.sum(pred == target).item()
             n_total += target.size(0)
-            running_loss += loss
+            running_loss += loss.item()
 
     avg_acc = 100 * (n_correct / n_total)
     avg_loss = running_loss / len(loaders[1])
-    print(f'\tEval: Avg Acc={avg_acc:.2f}, Avg Loss={avg_loss:.4f}')
+    print(f'Eval: Avg Acc={avg_acc:.4f}, Avg Loss={avg_loss:.4f}')
     return avg_acc, avg_loss
 
 
 def plot(acc_list, loss_list, dev_acc_list, dev_loss_list, lrs):
     def plot_acc():
+        plt.figure()
         plt.plot(acc_list, '-o')
         plt.plot(dev_acc_list, '-o')
         plt.xlabel('step')
@@ -159,6 +160,7 @@ def plot(acc_list, loss_list, dev_acc_list, dev_loss_list, lrs):
         plt.savefig('plots/acc_trend.png', dpi=500)
 
     def plot_loss():
+        plt.figure()
         plt.plot(loss_list, '-o')
         plt.plot(dev_loss_list, '-o')
         plt.xlabel('step')
@@ -169,6 +171,7 @@ def plot(acc_list, loss_list, dev_acc_list, dev_loss_list, lrs):
         plt.savefig('plots/loss_trend.png', dpi=500)
 
     def plot_lrs():
+        plt.figure()
         plt.plot(lrs)
         plt.xlabel('step')
         plt.ylabel('lr')
